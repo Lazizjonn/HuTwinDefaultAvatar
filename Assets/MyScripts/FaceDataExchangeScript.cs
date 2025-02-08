845c26142e38c589a85b3bde346d4a30cd31f07d
Zukhriddin Kamolov <ttymi.kamolov@gmail.com>
2025-02-08 14:08:44 +0100

    - Host player shares face expression data, but not client. No freezing or low frame rate, looks good, works nice. - Problem is bowl with tomato is invisible during runtime, but visible in Unity scene.

--- Assets/MyScripts/FaceDataExchangeScript.cs
+++ Assets/MyScripts/FaceDataExchangeScript.cs
@@ -12,8 +12,8 @@ public class FaceDataExchangeScript : NetworkBehaviour
 
     [SerializeField] public GameObject myPlayer;
 
-    private CorrectivesFace myFaceObject;
-    private CorrectivesFace hisFaceObject;
+    private CorrectivesFace[] myFaceObject;
+    private CorrectivesFace[] hisFaceObject;
     private float[] myExpressions;
 
     void Start()
@@ -21,7 +21,7 @@ public class FaceDataExchangeScript : NetworkBehaviour
         hostFaceShareScript = GameObject.FindGameObjectWithTag("FaceShareTransitionObj").GetComponentInChildren<HostShareScript>();
         clientFaceShareScript = GameObject.FindGameObjectWithTag("FaceShareTransitionObj").GetComponentInChildren<ClientShareScript>();
 
-        myFaceObject = myPlayer.GetComponentInChildren<CorrectivesFace>();
+        myFaceObject = myPlayer.GetComponentsInChildren<CorrectivesFace>();
         //Debug.LogError("--- Start(), this.GetInstanceID: " + this.GetInstanceID() + ", this.NetworkObjectId" + this.NetworkObjectId);
     }
 
@@ -38,12 +38,12 @@ public class FaceDataExchangeScript : NetworkBehaviour
         {
             if (player.GetComponent<NetworkObject>().IsLocalPlayer == true)
             {
-                myFaceObject = player.GetComponentInChildren<CorrectivesFace>();
+                myFaceObject = player.GetComponentsInChildren<CorrectivesFace>();
                 //Debug.LogError("--- + OnNewClientConnected(), myFaceObject is Null: " + (myFaceObject == null) + ", this.GetInstanceID: " + this.GetInstanceID() + ", this.NetworkObjectId" + this.NetworkObjectId);
             }
             else
             {
-                hisFaceObject = player.GetComponentInChildren<CorrectivesFace>();
+                hisFaceObject = player.GetComponentsInChildren<CorrectivesFace>();
                 //Debug.LogError("--- + OnNewClientConnected(), hisFaceObject is Null: " + (hisFaceObject == null) + ", this.GetInstanceID: " + this.GetInstanceID() + ", this.NetworkObjectId" + this.NetworkObjectId);
             }
         }
@@ -51,9 +51,9 @@ public class FaceDataExchangeScript : NetworkBehaviour
 
     private void UpdateFaceExpression()
     {
-        if (NetworkManager.Singleton != null && myFaceObject != null /*&& hisFaceObject != null*/)                 // ===================================
+        if (NetworkManager.Singleton != null && myFaceObject != null && hisFaceObject != null)
         {
-            myExpressions = myFaceObject.PrepareRemoteExpressionWeights();
+            myExpressions = myFaceObject[0].PrepareRemoteExpressionWeights();
             Debug.LogError("--- UpdateFaceExpression(), myExpressions.array.Length: " + myExpressions.Length);
             
             if (NetworkManager.Singleton.IsHost)
@@ -73,7 +73,8 @@ public class FaceDataExchangeScript : NetworkBehaviour
         hostFaceShareScript.SetFaceData(hostData);
 
         float[] data = clientFaceShareScript.GetFaceData();
-        hisFaceObject.UpdateExpressionWeightFromRemote(data);
+        hisFaceObject[0].UpdateExpressionWeightFromRemote(data);
+        hisFaceObject[1].UpdateExpressionWeightFromRemote(data);
     }
 
     private void SetExpressionPlayerClient(float[] clientData)
@@ -84,6 +85,7 @@ public class FaceDataExchangeScript : NetworkBehaviour
         clientFaceShareScript.SetFaceData(clientData);
 
         float[] data = hostFaceShareScript.GetFaceData();
-        hisFaceObject.UpdateExpressionWeightFromRemote(data);
+        hisFaceObject[0].UpdateExpressionWeightFromRemote(data);
+        hisFaceObject[1].UpdateExpressionWeightFromRemote(data);
     }
 }
